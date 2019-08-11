c4e6a1fee491db21ab376797bb6488ca19b0edeb
TejaswiniJawake <Tejaswini-PC@Tejaswini>
2019-08-11 11:50:02 +0530

    initial commit

---
+++ Simple/src/JavaDemo.java
@@ -0,0 +1,7 @@
+class javaDemo{
+	public static void main(String args[]){
+		System.out.println("Hello Achal uygusbiasnbakjbduyadbkasjnhsbxajbxkxhjxbAZNJKaZN  \n How r u??");
+		System.out.println("How r u??");
+		
+	}	//to add two numbers 
+}
